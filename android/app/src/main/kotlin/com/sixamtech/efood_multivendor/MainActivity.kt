b01f1e308705998c7232a1a331cbf26a625864f2
Kamlesh Kumar <dev.slinfy@gmail.com>
2022-06-13 10:31:05 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/sixamtech/efood_multivendor/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.sixamtech.stack_food
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+class MainActivity : FlutterActivity() {
+
+//    @Override
+//    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine)
+//    {
+//        GeneratedPluginRegistrant.registerWith(flutterEngine);
+//        new MethodChannel (flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+//        .setMethodCallHandler(
+//        (call, result
+//    ) -> {
+//        // Your existing code
+//    }
+//        );
+//    }
+}
+
